18f17f58197d831e8e268586c5a7db1b0b2e0d70
Wes <wal2.designer@gmail.com>
2022-05-03 18:38:57 +0100

    adding header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+    return(
+        <header>
+            <div>
+                <img src="" alt="" />
+            </div>
+        </header>
+    )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
-import styles from '../styles/home.module.scss'
+import Head from "next/head";
+// import styles from "../styles/home.module.scss";
 
 export default function Home() {
-  return (
-    <h1 className={styles.title}>Hello next</h1>
-  )
+    return (
+        <>
+            <Head>
+                <title>Iniício | ig.news</title>
+            </Head>
+            <h1 >Hello next</h1>
+        </>
+    );
 }
