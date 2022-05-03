23e4f82095b96627dce25ddcea683e7f11f3026c
Wes <wal2.designer@gmail.com>
2022-05-03 19:56:07 +0100

    home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,28 @@
 import Head from "next/head";
-// import styles from "../styles/home.module.scss";
+import styles from "./styles/home.module.scss";
 
 export default function Home() {
-    return (
-        <>
-            <Head>
-                <title>Iniício | ig.news</title>
-            </Head>
-            <h1 >Hello next</h1>
-        </>
-    );
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world
+          </h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90</span>
+          </p>
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
+    </>
+  );
 }
