6c715b8b0f579c89d399597062f76aecd64b7c6c
Wes <wal2.designer@gmail.com>
2022-05-05 09:58:01 +0100

    adding fauna db

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,13 +1,38 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 
+import { query as q } from "faunadb";
+
+import { fauna } from "../../../services/fauna";
+
 export default NextAuth({
-  // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: "c4b5dc6ba2bcf574dd78",
       clientSecret: "62e9394e1e2642488bf9d6cd2767addada3f8a7f",
     }),
-    // ...add more providers here
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index("user_by_email"), q.Casefold(user.email))
+              )
+            ),
+            q.Create(q.Collection("users"), { data: { email } }),
+            q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email)))
+          )
+        );
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
---
+++ src/services/fauna.ts
@@ -0,0 +1,6 @@
+import { Client } from "faunadb";
+
+export const fauna = new Client({
+  secret: "fnAEl0H8W_AAQy6Rl1XNNzAje4Yw7t9BfsMbeF8w",
+  domain: "db.us.fauna.com",
+});
