ba0480bc52ed043f8a3b20dbef258ac331b77a71
Wes <wal2.designer@gmail.com>
2022-05-30 11:04:17 +0100

    setup jest

--- src/jest.config.js
+++ src/jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testIgnorePatterns: ["/node/modules", "/.next"],
+  setupFilesAfterEnv: ["<rootDir>/src/tests/setupTests.ts"],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
   },
+  testEnvironment: "jsdom",
 };
